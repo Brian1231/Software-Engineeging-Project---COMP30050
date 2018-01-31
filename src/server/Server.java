11bdf2e59f13d1a9a60cb5bdc13dd4fac0d2ab6e
Brian1231 <brianoleary1995@gmail.com>
2018-01-31 17:04:52 +0000

    init commit

---
+++ src/server/Server.java
@@ -0,0 +1,51 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Inet4Address;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
+
+public class Server {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Your IP is: " + Inet4Address.getLocalHost().getHostAddress().toString());
+		final ServerSocket server = new ServerSocket(50000);
+		System.out.println("Listening for connection on port " + server.getLocalPort() + " ...");
+		boolean serverActive = true;
+		while (serverActive){
+			Socket socket = server.accept();
+			String response;
+
+			//Read input from client
+			InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+			BufferedReader reader = new BufferedReader(isr);
+			String line = reader.readLine();
+			if (!line.isEmpty()) {
+				System.out.println("Message from client: " + line);
+			}
+
+			switch (line.toLowerCase().split(" ")[0]){
+			case "exit":
+				server.close();
+				serverActive = false;
+				response = "Bye!";
+				break;
+			case "roll":
+				response = "Player rolled!";
+				break;
+			default:
+				response = JOptionPane.showInputDialog("Message from server", "hello from server");
+			}
+
+			//Send response
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			out.println(response);
+		}
+		System.out.println("Done!");
+	}
+}
