af0498c58d4c59976aed6537359b5c5beace3cf3
Brian1231 <brianoleary1995@gmail.com>
2018-02-02 18:00:26 +0000

    Basic Desktop app to connect to server

---
+++ desktop/src/client/Client.java
@@ -0,0 +1,33 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Inet4Address;
+import java.net.Socket;
+
+class Client {
+
+	final static String IP = "34.244.13.223";
+	final static int PORT = 8080;
+	final static String message = "Hello";
+	
+	public static void main(String[] args) throws IOException {
+		Socket client = new Socket(Inet4Address.getByName(IP), 8080);
+		
+		System.out.println("Sending \"" + message + "\" to: " + IP + " : " + PORT);
+		PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+		out.println("Hello");
+		
+		InputStreamReader isr = new InputStreamReader(client.getInputStream());
+		BufferedReader reader = new BufferedReader(isr);
+		String line = reader.readLine();
+		if (!line.isEmpty()) {
+			System.out.println("Response from Server: " + line);
+		}
+		
+		client.close();
+	}
+
+}
