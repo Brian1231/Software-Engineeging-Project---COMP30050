c3c5cee9686be637d69cc72f20c66e9503ab3fe4
Jose Collado San Pedro <jocolsan@inf.upv.es>
2018-02-22 18:02:43 +0000

    first connection ( join game)

--- android/SEGameController/app/src/main/java/pw/jcollado/segamecontroller/joinActivity/joinActivity.kt
+++ android/SEGameController/app/src/main/java/pw/jcollado/segamecontroller/joinActivity/joinActivity.kt
@@ -2,12 +2,39 @@ package pw.jcollado.segamecontroller.joinActivity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_join.*
 import pw.jcollado.segamecontroller.R
 
-class joinActivity : AppCompatActivity() {
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import pw.jcollado.segamecontroller.connections.AsyncResponse
+import pw.jcollado.segamecontroller.connections.ClientThread
+import pw.jcollado.segamecontroller.model.Connections
+
+
+class joinActivity : AppCompatActivity(),AsyncResponse {
+    override fun processFinish(output: String?) {
+        handleResponse(output)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+        joinButton.onClick { joinServer() }
+    }
+
+    fun joinServer(){
+
+        val thread = ClientThread(this@joinActivity)
+
+        thread.execute("{ \"id\": -1 }", Connections.IP.value, Connections.PORT.value)
+    }
+
+    private fun handleResponse(s: String?) {
+
+        when (s) {
+            null -> idTextView.text = "null"
+            "" -> idTextView.text = "Blank response!"
+            else -> idTextView.text = s
+        }
     }
 }
