8e6b0c7e4b2792a49852a6a4c96cea553c8f9988
Jose Collado San Pedro <jocolsan@inf.upv.es>
2018-03-22 20:58:50 +0100

    diagram and android app

--- android/SEGameController/app/src/main/java/pw/jcollado/segamecontroller/mainActivity/MainActivity.kt
+++ android/SEGameController/app/src/main/java/pw/jcollado/segamecontroller/mainActivity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 import pw.jcollado.segamecontroller.R
 import pw.jcollado.segamecontroller.connections.AsyncResponse
 import pw.jcollado.segamecontroller.listPropertiesActivity.ListPropertiesActivity
@@ -17,19 +18,18 @@ import pw.jcollado.segamecontroller.utils.requestToServer
 class MainActivity : App(), AsyncResponse {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setActionBar()
 
-
     }
 
 
-    fun onRoll(){
-        requestToServer(Request("roll","").toJSONString())
+    fun onRoll() {
+        requestToServer(Request("roll", "").toJSONString())
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.menu_main, menu)
@@ -40,19 +40,19 @@ class MainActivity : App(), AsyncResponse {
         // Handle item selection
         return when (item.itemId) {
             R.id.propertiesMenu -> {
-                startActivity(Intent(this,ListPropertiesActivity::class.java))
+                startActivity(Intent(this, ListPropertiesActivity::class.java))
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    private fun handleResponse(s: String?) {
+    private fun handleResponse(response: String?) {
 
-        when (s) {
-            null -> balanceTitle.text = "null"
-            "" -> balanceTitle.text = "Blank response!"
-            else -> balanceTitle.text = s
+        when (response) {
+            null -> response?.let { toast(it) }
+            "" -> toast(response)
+            else -> balanceTitle.text = response
         }
     }
 
