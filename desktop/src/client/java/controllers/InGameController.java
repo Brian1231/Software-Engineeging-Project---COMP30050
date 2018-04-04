a34b666cb8c0806562a186483b673b057f7d6bdb
andy-fitz <andrew.fitzgerald@ucdconnect.ie>
2018-04-04 18:07:02 +0100

    Removed some redundant code.

--- desktop/src/client/java/controllers/InGameController.java
+++ desktop/src/client/java/controllers/InGameController.java
@@ -7,18 +7,11 @@ import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.image.PixelWriter;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -145,74 +138,6 @@ public class InGameController {
         boardCanvas.draw();
     }
 
-    /*
-    // Populates the board with property. Plan to refactor a lot.
-    public void drawProperty() {
-
-        for (int i = 10; i >= 0; i--) {
-
-            squares.get(i).setMinSize(50, 75);
-            squares.get(i).setPrefSize(60, 100);
-
-            bottom.getChildren().add(squares.get(i));
-        }
-
-        for (int i = 19; i >= 11; i--) {
-
-            squares.get(i).setMinSize(75, 50);
-            squares.get(i).setPrefSize(100, 60);
-
-            left.getChildren().add(squares.get(i));
-        }
-
-        for (int i = 20; i < 31; i++) {
-
-            squares.get(i).setMinSize(50, 75);
-            squares.get(i).setPrefSize(60, 100);
-
-            top.getChildren().add(squares.get(i));
-        }
-
-        for (int i = 31; i < 40; i++) {
-
-            squares.get(i).setMinSize(75, 50);
-            squares.get(i).setPrefSize(100, 60);
-
-            right.getChildren().add(squares.get(i));
-        }
-    }
-
-
-    public void createSquares(){
-        for(int i = 0;i<40;i++){
-            Pane squarePane = new Pane();
-            squarePane.setStyle("-fx-background-color: #F2F4F4;" + "-fx-border-color: #34495E;");
-
-            VBox.setVgrow(squarePane, Priority.ALWAYS);
-            HBox.setHgrow(squarePane, Priority.ALWAYS);
-
-            Label name = new Label(SquareNames[i]);
-            //name.setRotate(90.0);
-            name.setPrefWidth(60);
-            name.setWrapText(true);
-            name.setStyle("-fx-text-fill: #34495E;" + "-fx-font-size: 7pt;");
-
-            name.layoutYProperty().bind(squarePane.heightProperty().subtract(name.heightProperty()).divide(2));
-            name.layoutXProperty().bind(squarePane.widthProperty().subtract(name.widthProperty()).divide(2));
-
-            squarePane.getChildren().add(name);
-            squares.add(squarePane);
-        }
-    }
-
-    // Draws players in their current positions.
-    public void drawPlayer(Pane square) {
-        Circle player = new Circle(30.0, 30.0, 10.0);
-        player.setFill(Color.BLUE);
-
-        square.getChildren().add(player);
-    }
-     */
     public void addPlayer(Player player){
         if(!players.contains(player)) {
             players.add(player);
