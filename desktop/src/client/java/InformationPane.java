6e03de067a9241276c62a74e0e651b243e65805f
andy-fitz <andrew.fitzgerald@ucdconnect.ie>
2018-04-12 16:02:44 +0100

    Added Tile info in 2nd loop. Added Game Title.

--- desktop/src/client/java/InformationPane.java
+++ desktop/src/client/java/InformationPane.java
@@ -1,35 +1,29 @@
 package client.java;
 
-import javafx.beans.property.DoubleProperty;
 import javafx.scene.control.TextArea;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.effect.Glow;
-import javafx.scene.effect.Shadow;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.control.Label;
 
-import java.awt.*;
-import java.util.ArrayList;
 
 public class InformationPane extends Pane {
 
-    ArrayList<Player> players = new ArrayList<>();
-    Circle eventLogger = new Circle();
-    TextArea feed = new TextArea();
+    private Label title = new Label("Panopoly");
+    private Circle eventLogger = new Circle();
+    private TextArea feed = new TextArea();
+    private Circle tileInfo  = new Circle();
+    private Label tileName = new Label("Name of current Tile");
+    private Label tileCost = new Label("$200");
 
     public InformationPane(){
+        //Title
+        title.setTextFill(Color.rgb(232, 142, 39));
+        title.setStyle("-fx-font-size: 50px;");
+        title.layoutXProperty().bind(widthProperty().divide(2).subtract(title.widthProperty().divide(2)));
+        getChildren().add(title);
 
-        // Effects
-        //Shadow s = new Shadow(5,Color.ORANGE);
-        //DropShadow ds = new DropShadow();
-        //ds.setOffsetX(0);
-        //ds.setOffsetY(0);
-        //ds.setColor(Color.WHITE);
-
-        // Circle
+        // Circle text Area (currently invisible)
         eventLogger.setFill(Color.rgb(60,67,79,0));
         eventLogger.setStroke(Color.rgb(119,137,165, 0));
         eventLogger.radiusProperty().bind(widthProperty().divide(9));
@@ -44,15 +38,38 @@ public class InformationPane extends Pane {
         feed.layoutYProperty().bind(heightProperty().divide(2).subtract(feed.prefHeightProperty().divide(2)));
         feed.setEditable(false);
         feed.setWrapText(true);
-
         feed.appendText("Welcome to Interdimensional Panopoly!\n");
         feed.appendText("Press the start button when all players have joined.\n");
         getChildren().add(feed);
+
+        // Current players location info
+        tileInfo.layoutXProperty().bind(widthProperty().subtract(widthProperty().divide(4.2)));
+        tileInfo.layoutYProperty().bind(heightProperty().divide(2));
+        tileInfo.setFill(Color.BLACK);
+        tileInfo.setStroke(Color.GOLD);
+        tileInfo.radiusProperty().bind(widthProperty().divide(9));
+        getChildren().add(tileInfo);
+            // Tile name
+        tileName.layoutXProperty().bind(tileInfo.layoutXProperty().subtract(tileName.widthProperty().divide(2)));
+        tileName.layoutYProperty().bind(tileInfo.layoutYProperty().subtract(tileInfo.radiusProperty().divide(2)));
+        tileName.setTextFill(Color.WHITE);
+        getChildren().add(tileName);
+            // Tile Cost
+        tileCost.layoutXProperty().bind(tileInfo.layoutXProperty().subtract(tileCost.widthProperty().divide(2)));
+        tileCost.layoutYProperty().bind(tileInfo.layoutYProperty().add(tileInfo.radiusProperty().divide(2)));
+        tileCost.setTextFill(Color.WHITE);
+        getChildren().add(tileCost);
+
     }
 
     public void updateFeed(String s){
         feed.appendText(s + "\n");
     }
 
+    public void updateLocationInfo(Location loc){
+        tileName.setText(loc.getName());
+        tileInfo.setStroke(loc.getColour());
+        tileCost.setText("$" + Integer.toString(loc.getPrice()));
+    }
 
 }
