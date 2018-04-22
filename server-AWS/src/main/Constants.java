9b7b55eb9f0891ddfe5988669a361bb941961ed4
Conal O'Neill <conal.oneill95@gmail.com>
2018-04-22 21:00:06 +0100

    fixed constants - added 2 more investments to match our game all colour groups now have 3 properties

--- server-AWS/src/main/Constants.java
+++ server-AWS/src/main/Constants.java
@@ -9,48 +9,87 @@ public final class Constants {
     }
 
 
-    // 24 investment
-    // 3 tax
-    // 4 stations
-    // 2 utils
-    // 3 chance
 
-    // Property rent constants - 28 total including 2 utils at index 7 & 20
-    public static final int[][] SITE_RENTS = {
-        {2,10,30,90,160,250},{4,20,60,180,320,450},{25,50,100,200,200,200},
+    // Investment property price constants - 24 total - each row is a new colour group
+    public static final int[] INVESTMENT_PRICES = {
+        60,60,60,
+        100,100,120,
+        140,140,160,
+        180,180,200,
+        220,220,240,
+        260,260,280,
+        300,300,320,
+        350,350,400
+    };
+
+    // Utilities price constants
+    public static final int[] UTILITY_PRICES = {
+        150,150
+    };
+
+    // Station price constants
+    public static final int[] STATION_PRICES = {
+        200,200,200,200
+    };
+
+    // Investment property rent constants - 24 total - each row is a new colour group
+    public static final int[][] INVESTMENT_RENTS = {
+        {2,10,30,90,160,250},{2,10,30,90,160,250},{4,20,60,180,320,450},
         {6,30,90,270,400,550},{6,30,90,270,400,550},{8,40,100,300,450,600},
-        {10,50,150,450,625,750},{4,10,0,0,0,0},{10,50,150,450,625,750},
-        {12,60,180,500,700,900},{25,50,100,200,200,200},{14,70,200,550,750,950},
-        {14,70,200,550,750,950},{16,80,220,600,800,1000},{18,90,250,700,875,1050},
-        {18,90,250,700,875,1050},{20,100,300,750,925,1100},{25,50,100,200,200,200},
-        {22,110,330,800,975,1150},{22,110,330,800,975,1150},{4,10,0,0,0,0},
-        {22,120,360,850,1025,1200},{26,130,390,900,1100,1275},
-        {26,130,390,900,1100,1275},{28,150,450,1000,1200,1400},{25,50,100,200,200,200},
-        {35,175,500,1100,1300,1500}, {50,200,600,1400,1700,2000}
+        {10,50,150,450,625,750},{10,50,150,450,625,750},{12,60,180,500,700,900},
+        {14,70,200,550,750,950},{14,70,200,550,750,950},{16,80,220,600,800,1000},
+        {18,90,250,700,875,1050},{18,90,250,700,875,1050},{20,100,300,750,925,1100},
+        {22,110,330,800,975,1150},{22,110,330,800,975,1150},{22,120,360,850,1025,1200},
+        {26,130,390,900,1100,1275},{26,130,390,900,1100,1275},{28,150,450,1000,1200,1400},
+        {35,175,500,1100,1300,1500},{35,175,500,1100,1300,1500},{50,200,600,1400,1700,2000}
+    };
+
+    // Utilities rent constants - these are the dice multipliers
+    public static final int[][] UTILITY_RENTS = {
+        {4,10},{4,10}
+    };
+
+    // Station rent constants  - 0-3 indices for 1-4 stations possible
+    public static final int[][] STATION_RENTS = {
+        {25,50,100,200},{25,50,100,200},
+        {25,50,100,200},{25,50,100,200}
     };
 
 
-    // Property price constants - 28 total including 2 utils at index 7 & 20
-    public static final int[] SITE_PRICES = {
-        60,60,200,100,100,120,140,150,140,160,200,180,180,200,
-        220,220,240,200,260,260,150,280,300,300,320,200,350,400
+    // Investment property house prices(hotels same price) - 24 total - each row is a new colour group
+    public static final int[] HOUSE_PRICES = 	{
+        30,30,30,
+        50,50,50,
+        100,100,100,
+        100,100,100,
+        150,150,150,
+        150,150,150,
+        150,150,160,
+        200,200,200
     };
 
-    // Property house prices(hotels same price) - 22 total
-    private static final int[] HOUSE_PRICES = 	{
-        30, 30, 50, 50, 50,
-        100,100,100,100,100,100,
-        150,150,150,150,150,150,
-        150,150,160,200,200
+
+    // Investment property mortgage constants - 24 total - each row is a new colour group
+    public static final int[] INVESTMENT_MORTGAGE_VALUE = {
+        50,50,50,
+        50,50,60,
+        70,70,80,
+        90,90,100,
+        110,110,120,
+        150,150,150,
+        200,200,200,
+        175,175,200
+    };
+
+    // Station mortgage value constants
+    public static final int[] STATION_MORTGAGE_VALUE = {
+        100,100,100,100
     };
 
 
-    // Property mortgage constants - 28 total including 2 utils at index 7 & 20
-    private static final int[] SITE_MORTGAGE_VALUE = {
-        50,50,100,50,50,60,70,
-        75,70,80,100,90,90,100,110,
-        110,120,100,150,150,75,150,
-        200,200,200,100,175,200
+    // Utility mortgage value constants
+    public static final int[] UTILITY_MORTGAGE_VALUE = {
+        75,75
     };
 
 
