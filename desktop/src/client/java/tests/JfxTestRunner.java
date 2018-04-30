fef2c8e89c9ea8fd6bec50f79dbeeda87f691706
Conal O'Neill <conal.oneill95@gmail.com>
2018-04-30 21:54:39 +0100

    added a test suite to desktop

--- desktop/src/client/java/tests/JfxTestRunner.java
+++ desktop/src/client/java/tests/JfxTestRunner.java
@@ -7,43 +7,26 @@ package client.java.tests;
 
 import java.util.concurrent.CountDownLatch;
 
-import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
 
 import com.sun.javafx.application.PlatformImpl;
 
 /**
- * This runner can be used to run JUnit-Tests on the JavaFx-Thread. This class can be used as a parameter to
- * the {@link RunWith} annotation. Example: *
- *
- * <pre>
- * <code>
- * &#64;RunWith( JfxTestRunner.class )
- * public class MyUnitTest
- * {
- *   &#64;Test
- *   public void testMyMethod()
- *   {
- *    //...
- *   }
- * }
- * </code>
- * </pre>
- *
- * @author okr
- * @date 18.11.2015
- */
+ * This runner is used to run JUnit-Tests on the JavaFx-Thread.
+ * This class is passed as a parameter with the @RunWith annotation
+ **/
+
 @SuppressWarnings("restriction")
 public class JfxTestRunner extends BlockJUnit4ClassRunner {
     /**
      * Creates a test runner, that initializes the JavaFx runtime.
      *
-     * @param klass The class under test.
+     * @param aClass The class under test.
      * @throws InitializationError if the test class is malformed.
      */
-    public JfxTestRunner(final Class<?> klass) throws InitializationError {
-        super(klass);
+    public JfxTestRunner(final Class<?> aClass) throws InitializationError {
+        super(aClass);
         try {
             setupJavaFX();
         } catch (final InterruptedException e) {
---
+++ desktop/src/client/java/tests/TestSuite.java
@@ -0,0 +1,16 @@
+package client.java.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    LocationTest.class,
+    VillainGangTest.class,
+    PlayerTest.class
+})
+
+public class TestSuite {
+}
