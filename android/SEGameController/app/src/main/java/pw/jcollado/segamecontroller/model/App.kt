7700bffcbf13f4fad2f9296883e86683e4b1401e
Jose Collado San Pedro <jocolsan@inf.upv.es>
2018-05-02 18:34:59 +0100

    phone vibrates when it's the turn.

--- android/SEGameController/app/src/main/java/pw/jcollado/segamecontroller/model/App.kt
+++ android/SEGameController/app/src/main/java/pw/jcollado/segamecontroller/model/App.kt
@@ -1,14 +1,16 @@
 package pw.jcollado.segamecontroller.model
 
-import android.app.ProgressDialog
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import kotlinx.android.synthetic.main.activity_list_properties.*
 import org.jetbrains.anko.toast
 import org.json.JSONArray
 import org.json.JSONObject
+import pw.jcollado.segamecontroller.R
 import pw.jcollado.segamecontroller.connections.AsyncResponse
 import pw.jcollado.segamecontroller.connections.ServerConnectionThread
 import pw.jcollado.segamecontroller.listPropertiesActivity.ListPropertiesActivity
@@ -78,34 +80,51 @@ abstract class App : AppCompatActivity(), AsyncResponse {
 
     fun parseResponsetoPlayer(response: String) {
         val responseJSON = JSONObject(response)
-        val charactername = responseJSON.getString("character")
 
-        Player.character = charactername
-        Player.balance = responseJSON.getInt("balance")
-        Player.fuel = responseJSON.getInt("fuel")
-        Player.id = responseJSON.getInt("id")
-        Player.colour = responseJSON.getInt("colour")
-        Player.position = responseJSON.getString("position")
-        Player.action_info.add(responseJSON.getString("action_info"))
-
-        preferences.character = charactername
-        if (!Player.properties.isEmpty()) {
-            Player.properties.clear()
-        }
-
-        val jArray = responseJSON.getJSONArray("properties") as JSONArray
-        if (jArray != null) {
-            for (i in 0 until jArray.length()) {
-                RequestFunctions().propertyFromJSONString(jArray.getString(i))?.let {
-                    Player.properties.add(it)
-                }
+        if (responseJSON.has("action") && responseJSON.getString("action") == "vibrate" && responseJSON.getInt("id") == Player.id){
+            runOnUiThread{
+            toast(getString(R.string.your_turn))
+            }
+            var v:Vibrator =  getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            // Vibrate for 500 milliseconds
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                v.vibrate(VibrationEffect.createOneShot(300,VibrationEffect.DEFAULT_AMPLITUDE))
+            }else{
+                //deprecated in API 26
+                v.vibrate(300)
             }
         }
+        else {
+
+            val charactername = responseJSON.getString("character")
+
+            Player.character = charactername
+            Player.balance = responseJSON.getInt("balance")
+            Player.fuel = responseJSON.getInt("fuel")
+            Player.id = responseJSON.getInt("id")
+            Player.colour = responseJSON.getInt("colour")
+            Player.position = responseJSON.getString("position")
+            Player.action_info.add(responseJSON.getString("action_info"))
+
+            preferences.character = charactername
+            if (!Player.properties.isEmpty()) {
+                Player.properties.clear()
+            }
+
+            val jArray = responseJSON.getJSONArray("properties") as JSONArray
+            if (jArray != null) {
+                for (i in 0 until jArray.length()) {
+                    RequestFunctions().propertyFromJSONString(jArray.getString(i))?.let {
+                        Player.properties.add(it)
+                    }
+                }
+            }
 
             updateGameState()
-            if(listActivity!=null){
+            if (listActivity != null) {
                 listActivity!!.updateProperties()
 
+            }
         }
 
     }
