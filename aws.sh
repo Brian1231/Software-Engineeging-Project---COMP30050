eb3ae37f0403ff62bd5d2ba5163803d0c4c9c64e
Conal O'Neill <conaloneill@users.noreply.github.com>
2018-05-07 22:38:15 +0100

    Update and rename aws.sh to se3-publicstaticboys.sh

--- aws.sh
+++
@@ -1,2 +0,0 @@
-cd server-AWS/
-ssh -i server.pem ubuntu@ec2-52-48-249-220.eu-west-1.compute.amazonaws.com
---
+++ se3-publicstaticboys.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/Brian1231/SEProject.git Desktop/SE3-PublicStaticBoys
+cd Desktop/SE3-PublicStaticBoys/server-AWS
+ssh -i server.pem ubuntu@ec2-52-48-249-220.eu-west-1.compute.amazonaws.com
